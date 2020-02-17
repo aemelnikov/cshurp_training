8199983a792cd9497c88ed74d8d39e0ed217a153
amelnikov <alexander.e.melnikov@gmail.com>
2020-02-17 15:43:47 +0300

    TestBase class has been added

---
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class GroupRemovaltests : TestBase
+    {
+
+        [Test]
+        public void GroupRemovalTest()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupPage();
+            Logout();
+        }
+
+
+
+       
+    }
+}
